63dbc11acda5489a9fcfc0fad67e08042fc9dd12
@Inori <2665611998@qq.com>
2022-02-12 23:56:08 +0800

    2.3.4(3, 萌百：优化启动屏动画效果)

--- app/src/main/java/com/moegirlviewer/screen/splash/state.kt
+++ app/src/main/java/com/moegirlviewer/screen/splash/state.kt
@@ -3,6 +3,7 @@ package com.moegirlviewer.screen.splash
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import com.moegirlviewer.util.SplashImage
+import com.moegirlviewer.util.isMoegirl
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.withContext
 
@@ -15,20 +16,36 @@ class SplashScreenState(
   internal val imageScale = Animatable(1.2f)
 
   suspend fun showAppearAnimation() = withContext(composeCoroutineScope.coroutineContext) {
-    contentAlpha.animateTo(
-      targetValue = 1f,
-      animationSpec = tween(
-        delayMillis = 1000,
-        durationMillis = 300
+    if (isMoegirl()) {
+      contentAlpha.animateTo(
+        targetValue = 1f,
+        animationSpec = tween(
+          delayMillis = 1000,
+          durationMillis = 300
+        )
       )
-    )
 
-    imageScale.animateTo(
-      targetValue = 1f,
-      animationSpec = tween(
-        durationMillis = 2800,
+      imageScale.animateTo(
+        targetValue = 1f,
+        animationSpec = tween(
+          durationMillis = 2800,
+        )
       )
-    )
+    } else {
+      contentAlpha.animateTo(
+        targetValue = 1f,
+        animationSpec = tween(
+          durationMillis = 350
+        )
+      )
+
+      imageScale.animateTo(
+        targetValue = 1f,
+        animationSpec = tween(
+          durationMillis = 2650,
+        )
+      )
+    }
   }
 
   suspend fun showTransparentAnimation() = withContext(composeCoroutineScope.coroutineContext) {
