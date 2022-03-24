c3e94ad9e72a19d180165ccb03dc04ddf55b1ddc
@Inori <2665611998@qq.com>
2022-03-24 19:52:02 +0800

    防止同时发送大量toast

--- app/src/main/java/com/moegirlviewer/util/toast.kt
+++ app/src/main/java/com/moegirlviewer/util/toast.kt
@@ -3,10 +3,16 @@ package com.moegirlviewer.util
 import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 private val scope = CoroutineScope(Dispatchers.Main)
+private var queueCount = 0
 
 fun toast(text: String) = scope.launch {
+  if (queueCount > 3) return@launch
   Toast.makeText(Globals.context, text, Toast.LENGTH_LONG).show()
+  queueCount++
+  delay(3000)
+  queueCount--
 }
